8ab23fecccdb7e664df99720ef76adca23b6c97e
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-09-09 02:30:15 +0300

    refactor: use slice split

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func main() {
 		var res string
 		switch {
 		case strings.HasPrefix(line, "POOL "):
-			_, cleanLine, _ := strings.Cut(line, " ")
+			cleanLine := line[5:]
 			backends := strings.Split(cleanLine, " ")
 			res = rr.Pool(backends)
 		case line == "PICK":
