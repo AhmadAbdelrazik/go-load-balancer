e9b4c474411f23118e31bb9a7aa7ff070b2debc2
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-09-09 02:55:26 +0300

    feat: Smooth Weighted Round-Robin

--- main.go
+++ main.go
@@ -4,55 +4,90 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type RoundRobin struct {
-	Backends []string
-	Pointer  int
+type Backend struct {
+	Name    string
+	Weight  int
+	Counter int
 }
 
-func (r *RoundRobin) Pool(backends []string) string {
+// TODO (weighted-rr): implement per the lesson description.
+type WeightedRoundRobin struct {
+	Backends     []Backend
+	TotalWeights int
+}
+
+func (r *WeightedRoundRobin) Pool(backends []Backend) string {
+	var totalWeight int
+	for _, b := range backends {
+		totalWeight += b.Weight
+	}
+
 	r.Backends = backends
-	r.Pointer = 0
+	r.TotalWeights = totalWeight
 	return "OK"
 }
 
-func (r *RoundRobin) Pick() string {
-	n := len(r.Backends)
-	if n == 0 {
-		return "EMPTY"
+func (r *WeightedRoundRobin) Pick() string {
+	var highestIdx int
+
+	for i, b := range r.Backends {
+		r.Backends[i].Counter += b.Weight // step 1: add the weights to counter
+
+		if r.Backends[i].Counter > r.Backends[highestIdx].Counter {
+			highestIdx = i
+		}
 	}
-	res := r.Backends[r.Pointer%n]
-	r.Pointer++
+
+	res := r.Backends[highestIdx].Name // step 2: use the highest counter
+
+	r.Backends[highestIdx].Counter -= r.TotalWeights
+
 	return res
 }
 
-func (r *RoundRobin) Reset() string {
-	r.Pointer = 0
-	return "OK"
+func (r *WeightedRoundRobin) PickN(n int) string {
+	results := make([]string, 0, n)
+	for range n {
+		results = append(results, r.Pick())
+	}
+
+	return strings.Join(results, ",")
 }
 
 func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
-	rr := &RoundRobin{}
+
+	wrr := &WeightedRoundRobin{}
 	for sc.Scan() {
 		line := sc.Text()
 		if line == "" {
 			continue
 		}
-		var res string
 		switch {
-		case strings.HasPrefix(line, "POOL "):
-			cleanLine := line[5:]
-			backends := strings.Split(cleanLine, " ")
-			res = rr.Pool(backends)
+		case strings.Contains(line, "POOL "):
+			cleanedLine := line[5:]
+			backendStrings := strings.Split(cleanedLine, " ")
+			backends := make([]Backend, 0, len(backendStrings))
+			for _, b := range backendStrings {
+				parts := strings.Split(b, ":")
+				weight, _ := strconv.Atoi(parts[1])
+				backend := Backend{
+					Name:   parts[0],
+					Weight: weight,
+				}
+				backends = append(backends, backend)
+			}
+			fmt.Println(wrr.Pool(backends))
+		case strings.Contains(line, "PICKN "):
+			n, _ := strconv.Atoi(line[6:])
+			fmt.Println(wrr.PickN(n))
 		case line == "PICK":
-			res = rr.Pick()
-		case line == "RESET":
-			res = rr.Reset()
+			fmt.Println(wrr.Pick())
 		}
-		fmt.Println(res)
 	}
 }
